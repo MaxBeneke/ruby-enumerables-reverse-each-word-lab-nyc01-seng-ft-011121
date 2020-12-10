11d6fc685f34d733805991aaa0aa7be9a74cfdb1
Max Beneke <max.beneke@gmail.com>
2020-12-10 15:42:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def reverse_each_word(string)
   new_array = string.split(" ")
+  final_array = []
   new_array.each do |word|
-    final_array = []
     final_array.push(word.reverse)
   end
  puts final_array
